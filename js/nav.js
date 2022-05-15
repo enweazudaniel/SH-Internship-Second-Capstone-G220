29c23bc7c223f10e9c91f713332fc50a1d3257cc
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-05-15 22:05:59 +0100

    Add eye icon

--- js/nav.js
+++ js/nav.js
@@ -39,3 +39,22 @@ if(Password1 != Password2) {
   document.write("JavaScript form has been submitted successfully");
 }
 }
+
+const togglePassword = document
+    .querySelector('#togglePassword');
+
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', () => {
+
+    // Toggle the type attribute using
+    // getAttribure() method
+    const type = password
+        .getAttribute('type') === 'password' ?
+        'text' : 'password';
+
+    password.setAttribute('type', type);
+
+    // Toggle the eye and bi-eye icon
+    this.classList.toggle('bi-eye');
+});
