244fb0be20f3bff30a7bc9934f31807398823807
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2022-05-17 15:26:01 +0100

    Refactor code

--- js/app.js
+++ js/app.js
@@ -8,30 +8,18 @@ function myFunction() {
 }
 function validateForm() {
 //check empty password field
-if(Password1 == "") {
+if(Password == "") {
   document.getElementById("message1").innerHTML = "**Fill the password please!";
   return false;
 }
 
 //check empty confirm password field
-if(Password2 == "") {
+if(Password1 == "") {
   document.getElementById("message2").innerHTML = "**Enter the password please!";
   return false;
 }
 
-//minimum password length validation
-if(Password1.length < 8) {
-  document.getElementById("message1").innerHTML = "**Password length must be atleast 8 characters";
-  return false;
-}
-
-//maximum length of password validation
-if(Password1.length > 15) {
-  document.getElementById("message1").innerHTML = "**Password length must not exceed 15 characters";
-  return false;
-}
-
-if(Password1 != Password2) {
+if(Password != Password1) {
   document.getElementById("message2").innerHTML = "**Passwords are not same";
   return false;
 } else {
